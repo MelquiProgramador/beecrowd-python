551a476f6fbee0bbbfbe7aa50574b614a201447d
Melqui Programador <121838328+MelquiProgramador@users.noreply.github.com>
2024-12-10 17:58:34 -0300

    Add files via upload

---
+++ beecrowd18.py
@@ -0,0 +1,10 @@
+# beecrowd - 1044
+valores = input().split()
+
+a = int(valores[0])
+b = int(valores[1])
+
+if a % b == 0 or b % a == 0:
+    print("Sao Multiplos")
+else:
+    print("Nao sao Multiplos")
