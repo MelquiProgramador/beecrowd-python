81449dfbef0116bf59e81e41405bd72f949f6357
Melqui Programador <121838328+MelquiProgramador@users.noreply.github.com>
2025-01-06 20:22:32 -0300

    Add files via upload

---
+++ beecrowd21.py
@@ -0,0 +1,15 @@
+# X = int(input())
+# Y = int(input())
+# soma = 0
+# for i in range(X,Y+1):
+#     if i % 13 == 2:
+#         pass
+#     else:
+#         soma = X
+
+# print(soma)
+
+#print(sum([i for i in range(int(input()), int(input()) + 1) if i % 13 != 0]))
+
+a = (sum([i for i in range(int(input()), int(input())+1) if i % 13 != 0]))
+print(a)        #                                                                !!!Não resolvido/não funciona no beecrowd!!!
